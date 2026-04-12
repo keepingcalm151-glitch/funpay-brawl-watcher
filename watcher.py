3222b4cdc9be3866c52ac2d25908e2375def5d7a
keepingcalm151-glitch <keepingcalm151@gmail.com>
2026-04-12 15:08:38 +0300

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -519,8 +519,8 @@ def get_price_range_for_heroes(heroes: int) -> Optional[tuple[float, float]]:
         return 100.0, 650.0
     if 95 <= heroes <= 99:
         return 100.0, 700.0
-    if 100 <= heroes <= 130:
-        return 100.0, 1000.0
+    if 100 <= heroes <= 120:
+        return 100.0, 1500.0
     return None
 
 
